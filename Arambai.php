dba588ec09009ddae86a32897d847bd5efe2be34
hkuser01 <46618154+hkuser01@users.noreply.github.com>
2019-01-12 18:59:14 +0800

    Update Arambai.php

--- Arambai.php
+++ Arambai.php
@@ -94,7 +94,7 @@
             <tr>
               <td class="tta1" style="width:10%">生命值</td>
               <td class="tta4">
-                <img class="gg" src="img/Bloodlines.png" width="30" alt=""><a href="Bloodlines.php.php">品種</a>(+20)</td>
+                <img class="gg" src="img/Bloodlines.png" width="30" alt=""><a href="Bloodlines.php">品種</a>(+20)</td>
             </tr>
             <tr>
               <td class="tta1" style="width:10%">攻擊力</td>
@@ -116,7 +116,7 @@
             <tr>
               <td class="tta1" style="width:10%">速度</td>
               <td class="tta4">
-                <img src="img/Husbandry.jpg" class="gg" alt=""><a href="Husbandry.php.php">畜牧培育</a>(+10%)</td>
+                <img src="img/Husbandry.jpg" class="gg" alt=""><a href="Husbandry.php">畜牧培育</a>(+10%)</td>
             </tr>
             <tr>
               <td class="tta1" style="width:10%">招降抵抗</td>
