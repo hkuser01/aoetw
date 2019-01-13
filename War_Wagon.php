8f0f6688c3c876472fd9fca44a2c63a19ba67b34
hkuser01 <46618154+hkuser01@users.noreply.github.com>
2019-01-13 20:55:30 +0800

    Update War_Wagon.php

--- War_Wagon.php
+++ War_Wagon.php
@@ -102,7 +102,7 @@
               <tr>
                 <td class="tta1" style="width:10%">開火速度</td>
                 <td class="tta4">
-                  <img class="gg" src="img/Thumb_Ring.png" width="30" alt=""><a href="Thumb _Ring.php">拇指環</a>(+11%)</td>
+                  <img class="gg" src="img/Thumb_Ring.png" width="30" alt=""><a href="Thumb_Ring.php">拇指環</a>(+11%)</td>
               </tr>
               <tr>
                 <td class="tta1" style="width:10%">命中率</td>
@@ -323,7 +323,7 @@
             <li><a href="Berbers.php">柏柏</a>：<a href="Kasbah.php">聚居堡壘</a>能夠提高馬戰車的生產速度還有加速
               <a
               href="Conscription.php">徵兵制度</a>的研究時間(快25%)</li>
-            <li><a href="Teutons.php">不列顛</a>：研究<a href="Thumb _Ring.php">拇指環</a>速度快20%。</li>
+            <li><a href="Teutons.php">不列顛</a>：研究<a href="Thumb_Ring.php">拇指環</a>速度快20%。</li>
             <li><a href="Teutons.php">匈奴</a>：研發<a href="Husbandry.php">畜牧培育</a>速度快20%。</li>
             <li><a href="Teutons.php">馬利</a>：研究<a href="Chemistry.php">化學</a>和<a href="Ballistic.php">彈道學</a>速度快80%。</li>
             <li><a href="Teutons.php">條頓</a>：馬戰車更不容易被招降。</li>
