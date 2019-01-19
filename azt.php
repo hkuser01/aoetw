04c9ed651bfbf3925daad0a056128cbfd82471e3
hkuser01 <46618154+hkuser01@users.noreply.github.com>
2019-01-19 19:15:22 +0800

    Update azt.php

--- azt.php
+++ azt.php
@@ -538,7 +538,7 @@ and (max-device-width: 480px)  {
     <table class="table" style="margin-top:50px;">
       <tr>
         <td class="trm" colspan="5" style="border-top: none;"> <a href="Mill.php"><img class="ng" src="img/Lumbercampicon.png" alt=""></a> 
-          <div><a href="Mill.php">伐木場</a>
+          <div><a href="Lumber_Camp.php">伐木場</a>
           </div>
         </td>
       </tr>
@@ -612,7 +612,7 @@ and (max-device-width: 480px)  {
         </td>
         <td class="t23 trm">
           <img class="gg" src="img/Stone_Shaft_Mining.jpg" alt="">
-          <div><a href="Stone_Shaft_Mining.phpp">礦井採石</a>
+          <div><a href="Stone_Shaft_Mining.php">礦井採石</a>
           </div>
         </td>
         <td class="t23 trm"></td>
@@ -928,7 +928,7 @@ and (max-device-width: 480px)  {
         </td>
         <td class="t23 trm not">
           <img class="gg" src="img/Guilds.jpg">
-          <div><a href="Guilds.phpp">行會制度</a>
+          <div><a href="Guilds.php">行會制度</a>
           </div>
         </td>
         <td class="t23 trm">
